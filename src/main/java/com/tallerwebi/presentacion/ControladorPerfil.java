5dffff6c98a8ff1800203033eaa2c315f2279d0f
Melina <violamelina.b@gmail.com>
2024-10-03 20:09:29 -0300

    Arreglos Generales V1

--- src/main/java/com/tallerwebi/presentacion/ControladorPerfil.java
+++ src/main/java/com/tallerwebi/presentacion/ControladorPerfil.java
@@ -6,11 +6,4 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
-@Controller
-public class ControladorPerfil {
 
-    @RequestMapping(path = "/perfil", method = RequestMethod.GET)
-    public ModelAndView irAPerfil() {
-        return new ModelAndView("perfil");
-    }
-}
